4a5701bae7d918c20a5d5bdf1674fa00731ec4a1
BhavyanshM <bhavyanshmishra@gmail.com>
2017-03-15 19:40:01 +0000

    Add an instance setup script for new instances used for DevOps research on EC2

---
+++ other/instance.setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+#sudo yum -y update
+#sudo yum install -y git
+#sudo yum install -y docker
+#sudo service docker start
+#sudo docker info
+#git clone http://github.com/BhavyanshM/DevOps.git
+cd DevOps
+git config --global user.name "BhavyanshM"
+git config --global user.email bhavyanshmishra@gmail.com
+
